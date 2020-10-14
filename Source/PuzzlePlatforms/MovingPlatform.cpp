6129932263c03f2c7dce35e15c8316f8274bdd43
pepperoni92 <jlapegna@gmail.com>
2020-10-14 16:39:49 -0700

    added basic platforms with server replication

---
+++ Source/PuzzlePlatforms/MovingPlatform.cpp
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MovingPlatform.h"
+
+AMovingPlatform::AMovingPlatform()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	SetMobility(EComponentMobility::Movable);
+}
+
+void AMovingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Set replication on server only
+	if (HasAuthority())
+	{
+		SetReplicates(true);
+		SetReplicateMovement(true);
+	}
+
+	GlobalStartLocation = GetActorLocation();
+	GlobalTargetLocation = GetTransform().TransformPosition(TargetLocation);
+
+}
+
+void AMovingPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	// return if this is not the server
+	if (!HasAuthority()) return;
+
+	FVector Location = GetActorLocation();
+
+	float TravelDistance = (GlobalTargetLocation - GlobalStartLocation).Size();
+	float DistanceTravelled = (Location - GlobalStartLocation).Size();
+
+	if (DistanceTravelled > TravelDistance)
+	{
+		FVector TempLocation = GlobalTargetLocation;
+		GlobalTargetLocation = GlobalStartLocation;
+		GlobalStartLocation = TempLocation;
+	}
+
+	FVector Direction = (GlobalTargetLocation - GlobalStartLocation).GetSafeNormal();
+	Location += Speed * DeltaTime * Direction;
+	SetActorLocation(Location);
+}
